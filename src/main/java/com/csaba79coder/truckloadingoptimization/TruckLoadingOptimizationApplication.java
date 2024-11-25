7972e75d388089965ac86607230caeb3821e8810
Csaba Vadász <csabavadasz79@gmail.com>
2024-11-25 09:17:39 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/truckloadingoptimization/TruckLoadingOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.truckloadingoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckLoadingOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckLoadingOptimizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/truckloadingoptimization/TruckLoadingOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.truckloadingoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckLoadingOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
